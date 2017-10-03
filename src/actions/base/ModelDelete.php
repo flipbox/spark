fd7024a64ba25f6e7b63b5a46b6ad1ba8ef85783
Nate Iler <nate@flipboxdigital.com>
2017-10-02 18:19:28 -0600

    fixing legacy namespaces

--- src/actions/base/ModelDelete.php
+++ src/actions/base/ModelDelete.php
@@ -2,10 +2,9 @@
 
 namespace flipbox\spark\actions\base;
 
-use flipbox\spark\actions\model\traits\Lookup;
 use yii\base\Action;
 
 abstract class ModelDelete extends Action
 {
-    use traits\DeleteAction, Lookup;
+    use traits\DeleteAction, traits\Lookup;
 }
--- src/actions/base/ModelUpdate.php
+++ src/actions/base/ModelUpdate.php
@@ -2,10 +2,9 @@
 
 namespace flipbox\spark\actions\base;
 
-use flipbox\spark\actions\model\traits\Lookup;
 use yii\base\Action;
 
 abstract class ModelUpdate extends Action
 {
-    use traits\SaveAction, Lookup;
+    use traits\SaveAction, traits\Lookup;
 }
